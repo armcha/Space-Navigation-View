6033e9dbf1fdb3f52547be2ae2de98880ee0a002
Chris <cmseuk@gmail.com>
2016-11-24 14:49:55 +0000

    added scroll aweness to bottom bar

--- spacelib/src/main/java/com/luseen/spacenavigation/SpaceNavigationViewBehavior.java
+++ spacelib/src/main/java/com/luseen/spacenavigation/SpaceNavigationViewBehavior.java
@@ -30,4 +30,23 @@ class SpaceNavigationViewBehavior<V extends View> extends CoordinatorLayout.Beha
         child.setTranslationY(translationY);
         return true;
     }
+
+    @Override
+    public boolean onStartNestedScroll (CoordinatorLayout coordinatorLayout, V child, View directTargetChild, View target, int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout, final V child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.setVisibility(View.GONE);  
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.setVisibility(View.VISIBLE);
+        }
+    }
 }
